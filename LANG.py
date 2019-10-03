065bb8f81a63d7ba2c6170fc4a73b61a7f35e46b
Alamius O <anton.obersteiner@gmail.com>
2019-10-03 11:26:51 +0200

    SPELL_SENTENCE spell_sentence: logical fixes and addition  - fixed comparator spell_sentence to count correctly  - added condition for parentless connectable sentence parts -> changed behaviour of final syllables

--- LANG.py
+++ LANG.py
@@ -193,15 +193,20 @@ def spell_sentence(sentence):
     for w in range(len(sentence)-1, -1, -1):
         word = sentence[w]
         if(
-            w > 1 and
+            w >= 1 and
             sentence[w-1].wtype["child_place_string"] ==
             sentence[w  ].wtype["parent_place_string"] and
             sentence[w-1].children == [sentence[w]] and
             sentence[w-1] in sentence[w  ].parents
         ):
-            word_strings = word_strings[:w-1] + [word_strings[w-1][:-2] + "-" + word_strings[w]] + word_strings[w+1:]
+            #the main verb loses its sentence structure endings and thus it has to be treated differently
+            if(word_strings[w-1][-2:] == sentence[w-1].wtype["child_place_string"][0]):
+                first_word = word_strings[w-1][:-2]
+            else:
+                first_word = word_strings[w-1]
+            word_strings = word_strings[:w-1] + [first_word + "-" + word_strings[w]] + word_strings[w+1:]
         elif(
-            w > 1 and
+            w >= 1 and
             sentence[w-1].wtype["child_place_string"] ==
             sentence[w  ].wtype["parent_place_string"] and
             sentence[w  ] in sentence[w-1].children and
